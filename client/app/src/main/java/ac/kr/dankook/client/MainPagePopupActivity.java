49e1b657a67706b7ebfe023b833887b6ce24d148
JhyunJ1 <JhyunJ1@gmail.com>
2023-04-04 20:59:28 +0900

    fix: add dialog effection

--- client/app/src/main/java/ac/kr/dankook/client/MainPagePopupActivity.java
+++ client/app/src/main/java/ac/kr/dankook/client/MainPagePopupActivity.java
@@ -1,30 +1,42 @@
 package ac.kr.dankook.client;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainPagePopupActivity extends AppCompatActivity {
 
     Button yesbtn,nobtn;
 
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         setContentView(R.layout.activity_main_page_popup);
         yesbtn = (Button)findViewById(R.id.button1);
         nobtn = (Button)findViewById(R.id.button2);
+        yesbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        nobtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
-    public void yes(View v) {
-        finish();
-    }
-
-    public void no(View v) {
-        finish();
-    }
 }
