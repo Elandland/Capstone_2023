1f12bb76943d819b4bd27d3df8fb520577f69d94
dmstn0526 <dmstn_0526@naver.com>
2023-05-01 17:05:35 +0900

    fixed 회원가입 항목 수정, 약관 동의 항목 추가

--- client/app/src/main/java/ac/kr/dankook/client/SignUpActivity.java
+++ client/app/src/main/java/ac/kr/dankook/client/SignUpActivity.java
@@ -12,13 +12,32 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatCheckBox;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    private EditText mNameEditText;
-    private EditText mPhoneNumEditText;
-    private EditText mEmailEditText;
-    private Button mSignupButton;
+    private EditText NickNameEditText;
+
+    private EditText PasswordEditText;
+
+    private Button SignupButton;
+
+    //체크박스 체크 여부 no check=0, check=1
+    private int TermsAgree0=0;
+    private int TermsAgree1=0;
+    private int TermsAgree2=0;
+    private int TermsAgree3=0;
+    private int TermsAgree4=0;
+    private int TermsAgree5=0;
+
+    //체크박스
+    AppCompatCheckBox check0; //전체 동의
+    AppCompatCheckBox check1; //첫번째 약관 동의
+    AppCompatCheckBox check2; //두번째
+    AppCompatCheckBox check3; //세번째
+    AppCompatCheckBox check4; //네번째
+    AppCompatCheckBox check5; //다섯번째
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,13 +46,12 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup_page);
         LoadViewById();
 
-        mSignupButton.setOnClickListener(new View.OnClickListener() {
+        SignupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name=mNameEditText.getText().toString();
-                String phoneNum=mPhoneNumEditText.getText().toString();
-                String email=mEmailEditText.getText().toString();
-                int result=RequestSignup(name,phoneNum,email);
+                String name=NickNameEditText.getText().toString();
+                String password=PasswordEditText.getText().toString();
+                int result=RequestSignup(name,password);
 
             }
         });
@@ -42,26 +60,18 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
 
-    private int RequestSignup(String name, String phoneNum, String email){
+    private int RequestSignup(String name, String password){
         return 0;
 
     }
     private void LoadViewById(){
-        if(mNameEditText == null){
-            mNameEditText = findViewById(R.id.signup_name_editText);
-            assert mNameEditText != null : "fail load nameEditText";
+        if(NickNameEditText == null){
+            NickNameEditText = findViewById(R.id.signup_nickname_editText);
+            assert NickNameEditText != null : "fail load nicknameEditText";
         }
-        if(mPhoneNumEditText == null){
-            mPhoneNumEditText = findViewById(R.id.signup_phoneNum_editText);
-            assert mPhoneNumEditText != null : "fail load phoneNum editText";
-        }
-        if(mEmailEditText == null){
-            mEmailEditText = findViewById(R.id.signup_email_editText);
-            assert mEmailEditText != null : "fail load emailEditText";
-        }
-        if(mSignupButton == null){
-            mSignupButton = findViewById(R.id.signup_button);
-            assert mSignupButton != null : "fail load signup button";
+        if(PasswordEditText == null){
+            PasswordEditText = findViewById(R.id.signup_password_editText);
+            assert PasswordEditText != null : "fail load password editText";
         }
     }
 }
