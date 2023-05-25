15f44316a799a2022b1a5e11fecfe4920e6772c5
Elandland <bodan99@gmail.com>
2023-05-25 21:48:25 +0900

    Feat : saveReview , deleteReview

--- server/src/main/java/Team/server/controller/ReviewController.java
+++ server/src/main/java/Team/server/controller/ReviewController.java
@@ -1,11 +1,12 @@
 package Team.server.controller;
 
 import Team.server.service.ReviewService;
+import Team.server.service.dto.ReviewDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,8 +16,11 @@ public class ReviewController {
     private final ReviewService reviewService;
 
 
-
-
+    @PostMapping("/write")
+    public ResponseEntity<Long> addReview(@RequestBody ReviewDto reviewDto){
+        Long reviewId = reviewService.wirtereview(reviewDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(reviewId);
+    }
 
 
     @DeleteMapping("/review/{no}")
