fe1265f96f2e919769edb6818b79fb344661b78b
leeminseokdankook <32133660+LMS1229@users.noreply.github.com>
2023-05-25 17:14:09 +0900

    fix: 매칭에 사용될 유저 데이터 필드 추가

--- server/src/main/java/Team/server/service/UserData.java
+++ server/src/main/java/Team/server/service/UserData.java
@@ -5,12 +5,14 @@ public class UserData {
     private int mLongitude;
     private int mMBTI;
     private String mUserID;
+    private int mLastMatchingTime;
 
-    public UserData(String userid, int latitude, int longitude, int mbti){
+    public UserData(String userid, int latitude, int longitude, int mbti, int last_matching_time){
         mUserID=userid;
         mLatitude=latitude;
         mLongitude=longitude;
         mMBTI=mbti;
+        mLastMatchingTime=last_matching_time;
     }
 
     public int GetLatitude(){
@@ -29,6 +31,13 @@ public class UserData {
         return mUserID;
     }
 
+    public int GetLastMatchingTime(){
+        return this.mLastMatchingTime;
+    }
+
+    public void SetLastMatchingTime(int currentLastMatchingTime){
+        this.mLastMatchingTime=currentLastMatchingTime;
+    }
     public void SetLatitude(int latitude){
         mLatitude=latitude;
     }
